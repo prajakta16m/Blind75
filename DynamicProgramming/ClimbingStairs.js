41a42eb4dbb84e30cc4c65530a03f2019bee036c
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2024-09-10 15:26:41 +0530

    ClimbingStairs - iterative

--- DynamicProgramming/ClimbingStairs.js
+++ DynamicProgramming/ClimbingStairs.js
@@ -5,3 +5,12 @@ var climbStairs = function(n) {
     }
     return climbStairs(n-1) + climbStairs(n-2);
 };
+
+//2. Using dp - iterative
+var climbStairs = function(n) {
+    let dp = [1,1];
+    for(let i=2; i<=n;i++){
+        dp.push(dp[i-1]+dp[i-2]);
+    }
+    return dp[n];
+};
